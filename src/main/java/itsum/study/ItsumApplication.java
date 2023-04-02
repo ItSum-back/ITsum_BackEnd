b3a5da2e3f94e5cfd71dca5415bd81f6f8917d90
DongJae <tlsehdwo7@naver.com>
2023-04-02 10:00:01 +0900

    Init: Dev/Prd 환경 구성

--- src/main/java/itsum/study/ItsumApplication.java
+++ src/main/java/itsum/study/ItsumApplication.java
@@ -1,13 +1,21 @@
 package itsum.study;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ItsumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItsumApplication.class, args);
+	}
+
+	@GetMapping
+	public String string(@Value("${greeting.message}") String message) {
+		return message;
 	}
 
 }
