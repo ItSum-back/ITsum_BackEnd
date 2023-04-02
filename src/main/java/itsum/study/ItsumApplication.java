90a906bdc12b420159943b6db1a5028761abe026
DongJae <tlsehdwo7@naver.com>
2023-04-02 09:15:50 +0900

    init project

---
+++ src/main/java/itsum/study/ItsumApplication.java
@@ -0,0 +1,13 @@
+package itsum.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itsum/study/ItsumApplicationTests.java
@@ -0,0 +1,13 @@
+package itsum.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
