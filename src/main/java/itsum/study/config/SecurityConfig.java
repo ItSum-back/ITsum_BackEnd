18a54f3120f8b245efe44b2948bbe26d882b6452
DongJae <tlsehdwo7@naver.com>
2023-04-02 17:55:30 +0900

    init: dev/prd 환경 구성 Swagger-Security 버전 차이로 인한 연동 오류 수정

---
+++ src/main/java/itsum/study/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package itsum.study.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+//Filter는 어디에 쓰이나?
+//필터는 주로 요청에 대한 인증, 권한 체크 등을 하는데에 쓰입니다. 구체적으로 들어온 요청이 DispatcherServlet에 전달되기 전에 헤더를 검사해 인증 토큰이 있는지 없는지, 올바른지 올바르지 않은지 등을 검사할 수 있을 것입니다.
+//
+//
+//
+//저는 개인 게시판 프로젝트에서 SpringSecurity와 Jwt를 이용한 인증을 구현하는 데에 있어 Filter를 사용한 경험이 있습니다.
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig {
+    private static final String[] PERMIT_URL_ARRAY = {
+            /* swagger v2 */
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            /* swagger v3 */
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
+
+    //Spring Security 5.4 이후부터는 WebSecurityConfigurerAdapter extends 불가능
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers(PERMIT_URL_ARRAY)
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        return http.build();
+    }
+}
---
+++ src/main/java/itsum/study/controller/AuthController.java
@@ -0,0 +1,21 @@
+package itsum.study.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class AuthController {
+//    private final KakaoAuthService kakaoAuthService;
+//    private final GoogleAuthService googleAuthService;
+//    private final AuthTokenProvider authTokenProvider;
+//    private final AuthService authService;
+//
+//    /**
+//     * 카카오 소셜 로그인 기능
+//     * @return ResponseEntity<AuthResponse>
+//     */
+//    @ApiOperation(value = "카카오 로그인", notes = "카카오 엑세스 토큰을 이용하여 사용자 정보 받아 저장하고 앱의 토큰 신환")
+//    @PostMapping(value = "/kakao")
+//    public ResponseEntity<AuthResponse> kakaoAuthRequest(@RequestBody AuthRequest authRequest) {
+//        return ApiResponse.success(kakaoAuthService.login(authRequest)); // body에 appToken 반환(response code 200)
+//    }
+}
