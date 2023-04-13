3e18545efad2d99011045f890c20ddd9891ea172
DongJae <tlsehdwo7@naver.com>
2023-04-13 21:38:08 +0900

    test

--- src/main/java/itsum/study/ItsumApplication.java
+++ src/main/java/itsum/study/ItsumApplication.java
@@ -4,17 +4,14 @@ package itsum.study;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class ItsumApplication {
-
-	@Bean
-	BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItsumApplication.class, args);
