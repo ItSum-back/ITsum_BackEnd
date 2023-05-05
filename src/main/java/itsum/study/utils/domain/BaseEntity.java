3ae214c2710fdbdd189b69074b1491160ac32837
DongJae <tlsehdwo7@naver.com>
2023-05-06 03:33:46 +0900

    feat : 등록 / 수정일 관련 공통 엔티티 추가

--- src/main/java/itsum/study/utils/domain/BaseEntity.java
+++ src/main/java/itsum/study/utils/domain/BaseEntity.java
@@ -1,29 +1,35 @@
 package itsum.study.utils.domain;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Getter(AccessLevel.PROTECTED)
+
+
+@Getter
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
 public abstract class BaseEntity {
-
     // 임의로 이름 지정
     @CreatedDate
-    @Column(name = "created_date_time", updatable = false)
-    private LocalDateTime createdDateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "last_modified_date_time")
-    private LocalDateTime lastModifiedDateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+
 }
 
